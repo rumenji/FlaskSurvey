5ea3f0d5895cad9eb3a7f88d08afb896c3d1c53c
rumenji <rumen_ji@yahoo.com>
2023-11-08 16:24:28 -0500

    Completed Flask Survey Exercise

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, request, redirect, flash
-from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, render_template, request, redirect, flash, session
+# from flask_debugtoolbar import DebugToolbarExtension
 from surveys import satisfaction_survey
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
-debug = DebugToolbarExtension(app)
+# debug = DebugToolbarExtension(app)
 
-RESPONSES = []
+# RESPONSES = []
 
 @app.route('/')
 def start_survey():
@@ -14,15 +14,21 @@ def start_survey():
     instructions = satisfaction_survey.instructions
     return render_template('home.html', title=title, instructions=instructions)
 
+@app.route('/set_session', methods = ['POST'])
+def set_session():
+    session['responses'] = []
+    return redirect ('/questions/0')
+
 @app.route('/questions/<int:question_id>')
 def questions(question_id):
     questions_list = satisfaction_survey.questions
-    if len(RESPONSES) < len(questions_list):
-        if question_id == len(RESPONSES):
+    responses = session.get('responses')
+    if len(responses) < len(questions_list):
+        if question_id == len(responses):
             return render_template('question.html', question=questions_list[question_id], question_id=question_id)
         else:
             flash("You need to answer the questions in order!", 'error')
-            return render_template('question.html', question=questions_list[len(RESPONSES)], question_id=len(RESPONSES))
+            return render_template('question.html', question=questions_list[len(responses)], question_id=len(session['responses']))
     else:    
         return redirect('/endsurvey')
     
@@ -33,7 +39,9 @@ def answers():
     # pdb.set_trace()
     answer = request.form['answer']
     next_question = request.form['id']
-    RESPONSES.append(answer)
+    responses = session.get('responses')
+    responses.append(answer)
+    session['responses'] = responses
     return redirect(f'questions/{int(next_question)+1}')
 
 @app.route('/endsurvey')
